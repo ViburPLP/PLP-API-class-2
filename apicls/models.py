a4410fb2eaf485ae48932e2bca53715b0913fba0
Vibur PLP <vickarish@gmail.com>
2024-04-29 21:54:34 +0300

    Finaly made the chart.

---
+++ apicls/models.py
@@ -0,0 +1,6 @@
+# from django.db import models
+
+# class Stockprice(models.Model):
+#     date = models.DateField()
+#     stock_symbol = models.CharField(max_length=10)
+#     price = models.FloatField() 
--- apicls/views.py
+++ apicls/views.py
@@ -1,22 +1,18 @@
 import requests
 from django.shortcuts import render
 from django.http import HttpResponse
+# from.models import Stockprice
 
 def dashboard (request):
     response = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=ZROA09JARY1NMF05')
     stock_data = response.json()
     result = stock_data['Time Series (Daily)']
 
+    dates = []
+    prices = []
+    for date, values in result.items():
+        dates.append(date)
+        prices.append(float(values["4. close"]))
 
-
-    if 'Time Series (Daily)' in stock_data:
-        stock_prices = []
-        for date, value in stock_data['Time Series (Daily)'].items():
-            stock_price = Stockprice.objects.create(
-                date=date,
-                stock_symbol='AAPL',
-                price = value['4. close'],
-            )
-
-    return render(request, 'dashboard.html' , {'result': result})
+    return render(request, 'dashboard.html' , {'dates': dates, 'prices': prices})
 
